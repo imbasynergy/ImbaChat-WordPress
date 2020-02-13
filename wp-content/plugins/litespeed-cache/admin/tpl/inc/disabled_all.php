7470b37be7789b3c226e073c7735c494d036c52b
Виталий <pupkinandroit@gmail.com>
2020-02-13 15:45:41 +1000

    update

--- wp-content/plugins/litespeed-cache/admin/tpl/inc/disabled_all.php
+++
@@ -1,8 +0,0 @@
-<?php
-if ( ! defined( 'WPINC' ) ) die ;
-
-$err = __( 'Disable All Features', 'litespeed-cache' ) ;
-
-// other plugin left cache expired rules in .htaccess which will cause conflicts
-echo LiteSpeed_Cache_Admin_Display::build_notice( LiteSpeed_Cache_Admin_Display::NOTICE_RED, $err ) ;
-
--- wp-content/plugins/litespeed-cache/tpl/esi.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if ( ! defined('ABSPATH') ) {
-    die() ;
-}
-
-LiteSpeed_Cache_ESI::get_instance()->load_esi_block() ;
-
-
