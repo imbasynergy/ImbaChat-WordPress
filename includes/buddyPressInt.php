d6f2c248cf22f11958a00129b0cecb1f936d97e6
Spray <danilkravciv@gmail.com>
2020-08-10 12:29:28 +0700

    add class to shortcode OPEN DIALOG

--- includes/buddyPressInt.php
+++ includes/buddyPressInt.php
@@ -7,5 +7,5 @@ add_action('bp_after_members_loop', function (){
 add_action('bp_member_header_actions', function (){
     $user_id = bp_displayed_user_id();
     if ($user_id != get_current_user_id())
-        echo do_shortcode('[ic_open_dialog id="'.$user_id.'"]');
+        echo do_shortcode('[ic_open_dialog id="'.$user_id.'" class="generic-button"]');
 });
