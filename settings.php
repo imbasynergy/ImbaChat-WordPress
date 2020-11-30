1e8352ae41a44e19cfa6cfb258ba1c267105626b
SprayDev <danilkravciv@gmail.com>
2020-11-30 17:54:20 +0700

    cron tab

--- settings.php
+++ settings.php
@@ -11,14 +11,14 @@ wp_enqueue_style( 'imbachat.css');
 wp_register_style( 'fontawesome', 'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
 wp_enqueue_style( 'fontawesome');
 
-    require_once( IMBACHAT__PLUGIN_DIR . '/includes/buddyPressInt.php' );
+require_once( IMBACHAT__PLUGIN_DIR . '/includes/buddyPressInt.php' );
 
-    require_once( IMBACHAT__PLUGIN_DIR . '/includes/wcfm_market_int.php' );
+require_once( IMBACHAT__PLUGIN_DIR . '/includes/wcfm_market_int.php' );
 
 if ( is_admin() ) {
     require_once IMBACHAT__PLUGIN_DIR . '/admin/admin.php';
 }
-
+require_once IMBACHAT__PLUGIN_DIR . '/view/users_table.php';
 add_action('plugins_loaded', 'imbachat');
 
 function imbachat(){
@@ -28,20 +28,20 @@ function imbachat(){
     add_shortcode( 'ic_open_chat', 'ic_open_chat' );
     add_shortcode( 'ic_close_chat', 'ic_close_chat' );
     add_shortcode( 'ic_wise_chat', 'ic_wise_chat');
-
+    add_shortcode('users_main_page', 'get_users_for_auth');
     if (get_option('IMCH_login','') == '' || get_option('IMCH_password','') == '' || get_option('IMCH_secret_key','') == '')
     {
         if (get_option('IMCH_dev_id', '') == '')
             $dev_id = null;
         else
             $dev_id = get_option('IMCH_dev_id');
-
+        //sync_with_imba_api($dev_id, $_SERVER['HTTP_HOST'], get_option( 'admin_email' ));
     }
 
     wp_register_script('IMCH_script', IC_PLUGIN_URL.'/view/imbachat.js','','', true);
     wp_enqueue_script( 'IMCH_script');
+    //wp_enqueue_script('my-amazing-script');
 }
-
 add_action('wp_footer', function()
 {
     if (get_option('IMCH_dev_id') == '')
@@ -55,81 +55,63 @@ add_action('wp_footer', function()
         update_option('IMCH_password', $IMCH_password);
         update_option('IMCH_secret_key', $IMCH_secret_key);
     }
+
     $dev_id = get_option('IMCH_dev_id');
     $json_data = IMCH_getJsSettingsString();
     require_once( IMBACHAT__PLUGIN_DIR . '/view/script.php' );
 });
 
-add_action('admin_menu',function ()
-{
-    add_options_page('ImbaChatWidget', 'ImbaChat Settings', 8, 'imbachat',function ()
-    {
+// добавляет новую крон задачу
+add_action( 'wp', 'imba_cron_activation' );
+function imba_cron_activation() {
+    wp_clear_scheduled_hook( 'imba_wp_stat' );
+    if( ! wp_next_scheduled( 'imba_wp_stat' ) ) {
+        wp_schedule_event( time(), 'daily', 'imba_wp_stat');
+    }
+}
 
-        add_option('IMCH_dev_id', '');
-        add_option('IMCH_login', '');
-        add_option('IMCH_password', '');
-        add_option('IMCH_secret_key', '');
-        add_option('IMCH_buddypress', '');
-        add_option('IMCH_market', '');
-        if(isset($_POST['IMCH_setting_setup']) && check_admin_referer( 'IMCH_setting_setup' ) && current_user_can('administrator')){
+// добавляем функцию к указанному хуку
+add_action( 'imba_wp_stat', 'do_imba_wp_stat' );
+function do_imba_wp_stat(){
+    try {
 
-            try {
+        $apl=get_option('active_plugins');
+        $apl = json_encode($apl);
 
-                $apl=get_option('active_plugins');
-                $apl = json_encode($apl);
+        $post_data = [
+            'host' => $_SERVER['HTTP_HOST'],
+            'lang' => get_locale(),
+            'name' => $_SERVER['SERVER_NAME'],
+            'plugins' => $apl,
+            'admin_mail' => get_option( 'admin_email' ),
+            'template' => get_option( 'template' ),
+            'widget_id' => sanitize_text_field(get_option('IMCH_dev_id'))
+        ];
+        $url = 'https://api.imbachat.com/imbachat/api/wp_stat';
+        $curl = curl_init();
 
-                $post_data = [
-                    'host' => $_SERVER['HTTP_HOST'],
-                    'lang' => get_locale(),
-                    'name' => $_SERVER['SERVER_NAME'],
-                    'plugins' => $apl,
-                    'admin_mail' => get_option( 'admin_email' ),
-                    'template' => get_option( 'template' ),
-                    'widget_id' => sanitize_text_field($_POST['IMCH_dev_id'])
-                ];
-                $url = 'https://api.imbachat.com/imbachat/api/wp_stat';
-                $curl = curl_init();
-
-                curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
-                curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE);
-                curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
-                curl_setopt($curl, CURLOPT_TIMEOUT, 5);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE);
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($curl, CURLOPT_TIMEOUT, 5);
 
 //                curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
 //                curl_setopt($curl, CURLOPT_USERPWD, $auth_password);
 
-                curl_setopt($curl, CURLOPT_URL, $url);
-                curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-                curl_setopt($curl, CURLOPT_POST, true);
-                curl_setopt($curl, CURLOPT_POSTFIELDS, $post_data);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $post_data);
 
-                //curl_setopt($curl, CURLOPT_POSTFIELDS, "users=".$arr);
+        //curl_setopt($curl, CURLOPT_POSTFIELDS, "users=".$arr);
 
 
-                $curlout = curl_exec($curl);
-                curl_close($curl);
-            } catch (Exception $exception){
+        $curlout = curl_exec($curl);
+        curl_close($curl);
+    } catch (Exception $exception){
 
-            }
-
-            $IMCH_dev_id = sanitize_text_field($_POST['IMCH_dev_id']);
-            $IMCH_login = sanitize_text_field($_POST['IMCH_login']);
-            $IMCH_password = $_POST['IMCH_password'];
-            $IMCH_secret_key = sanitize_text_field($_POST['IMCH_secret_key']);
-            $IMCH_buddypress = $_POST['IMCH_buddypress'];
-            $IMCH_market = $_POST['IMCH_market'];
-
-            update_option('IMCH_dev_id', $IMCH_dev_id);
-            update_option('IMCH_login', $IMCH_login);
-            update_option('IMCH_password', $IMCH_password);
-            update_option('IMCH_secret_key', $IMCH_secret_key);
-            update_option('IMCH_buddypress', $IMCH_buddypress);
-            update_option('IMCH_market', $IMCH_market);
-
-        }
-        require_once( IMBACHAT__PLUGIN_DIR . '/view/admin.php' );
-    });
-});
+    }
+}
 
 function IMCH_getJsSettingsString($opt = []) {
 
