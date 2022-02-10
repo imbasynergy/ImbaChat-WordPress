9fb030f93799310cc88d25082a748f44e4351ff5
Vadim <vad3724@gmail.com>
2022-02-10 13:16:49 +0300

    test seret

--- deploy.sh
+++ deploy.sh
@@ -1,143 +1,142 @@
 set -eo
-echo "$SEC_TEST ghhgj"
-## checking variable
-#if [[ -z "$SVN_USERNAME" ]]; then
-#	echo "Set the SVN_USERNAME secret"
-#	exit 1
+# checking variable
+if [[ -z "$SVN_USERNAME" ]]; then
+	echo "Set the SVN_USERNAME secret"
+	exit 1
+fi
+
+if [[ -z "$SVN_PASSWORD" ]]; then
+	echo "Set the SVN_PASSWORD secret"
+	exit 1
+fi
+
+# Allow some ENV variables to be customized
+if [[ -z "$SLUG" ]]; then
+	SLUG="imbachat-widget"
+fi
+echo "ℹ︎ SLUG is $SLUG"
+
+
+#if [[ -z "$VERSION" ]]; then
+#	VERSION="${GITHUB_REF#refs/tags/}"
+#	VERSION="${VERSION#v}"
 #fi
-#
-#if [[ -z "$SVN_PASSWORD" ]]; then
-#	echo "Set the SVN_PASSWORD secret"
-#	exit 1
-#fi
-#
-## Allow some ENV variables to be customized
-#if [[ -z "$SLUG" ]]; then
-#	SLUG="imbachat-widget"
-#fi
-#echo "ℹ︎ SLUG is $SLUG"
-#
-#
-##if [[ -z "$VERSION" ]]; then
-##	VERSION="${GITHUB_REF#refs/tags/}"
-##	VERSION="${VERSION#v}"
-##fi
-#
-#echo "ℹ︎ VERSION is $VERSION"
-#
-#if [[ -z "$ASSETS_DIR" ]]; then
-#	ASSETS_DIR=".wordpress-org"
-#fi
-#echo "ℹ︎ ASSETS_DIR is $ASSETS_DIR"
-#
-#SVN_URL="https://plugins.svn.wordpress.org/${SLUG}/"
-#SVN_DIR="${HOME}/svn-${SLUG}"
-#
-#echo "ℹ︎ SVN url is $SVN_URL"
-#echo "ℹ︎ SVN dir is $SVN_DIR"
-#
-## Checkout just trunk and assets for efficiency
-## Tagging will be handled on the SVN level
-#
-#echo "➤ Checking out .org repository..."
-#svn checkout --depth immediates "$SVN_URL" "$SVN_DIR"
-#cd "$SVN_DIR"
-#svn update --set-depth infinity assets
-#svn update --set-depth infinity trunk
-#
-#echo "➤ Copying files..."
-#if [[ -e "$GITHUB_WORKSPACE/.distignore" ]]; then
-#	echo "ℹ︎ Using .distignore"
-#	# Copy from current branch to /trunk, excluding dotorg assets
-#	# The --delete flag will delete anything in destination that no longer exists in source
-#	rsync -rc --exclude-from="$GITHUB_WORKSPACE/.distignore" "$GITHUB_WORKSPACE/" trunk/ --delete --delete-excluded
-#else
-#	echo "ℹ︎ Using .gitattributes"
-#
-#	cd "$GITHUB_WORKSPACE"
-#
-#	# "Export" a cleaned copy to a temp directory
-#	TMP_DIR="${HOME}/archivetmp"
-#	mkdir "$TMP_DIR"
-#
-#	git config --global user.email "vad3724@gmail.com"
-#	git config --global user.name "vad3724"
-#
-#	# If there's no .gitattributes file, write a default one into place
-#	if [[ ! -e "$GITHUB_WORKSPACE/.gitattributes" ]]; then
-#		cat > "$GITHUB_WORKSPACE/.gitattributes" <<-EOL
-#		/$ASSETS_DIR export-ignore
-#		/.gitattributes export-ignore
-#		/.gitignore export-ignore
-#		/.github export-ignore
-#		EOL
-#
-#		# Ensure we are in the $GITHUB_WORKSPACE directory, just in case
-#		# The .gitattributes file has to be committed to be used
-#		# Just don't push it to the origin repo :)
-#		git add .gitattributes && git commit -m "Add .gitattributes file"
-#	fi
-#
-#	# This will exclude everything in the .gitattributes file with the export-ignore flag
-#	git archive HEAD | tar x --directory="$TMP_DIR"
-#
-#	cd "$SVN_DIR"
-#
-#	# Copy from clean copy to /trunk, excluding dotorg assets
-#	# The --delete flag will delete anything in destination that no longer exists in source
-#	rsync -rc "$TMP_DIR/" trunk/ --delete --delete-excluded
-#fi
-#
-## Copy dotorg assets to /assets
-#if [[ -d "$GITHUB_WORKSPACE/$ASSETS_DIR/" ]]; then
-#	rsync -rc "$GITHUB_WORKSPACE/$ASSETS_DIR/" assets/ --delete
-#else
-#	echo "ℹ︎ No assets directory found; skipping asset copy"
-#fi
-#
-## Add everything and commit to SVN
-## The force flag ensures we recurse into subdirectories even if they are already added
-## Suppress stdout in favor of svn status later for readability
-#echo "➤ Preparing files..."
-#svn add . --force > /dev/null
-#
-## SVN delete all deleted files
-## Also suppress stdout here
-#svn status | grep '^\!' | sed 's/! *//' | xargs -I% svn rm %@ > /dev/null
-#
-## Copy tag locally to make this a single commit
-#echo "➤ Copying tag..."
-#svn cp "trunk" "tags/$SVN_VERSION"
-#
-## Fix screenshots getting force downloaded when clicking them
-## https://developer.wordpress.org/plugins/wordpress-org/plugin-assets/
-#if test -d "$SVN_DIR/assets" && test -n "$(find "$SVN_DIR/assets" -maxdepth 1 -name "*.png" -print -quit)"; then
-#    svn propset svn:mime-type "image/png" "$SVN_DIR/assets/*.png" || true
-#fi
-#if test -d "$SVN_DIR/assets" && test -n "$(find "$SVN_DIR/assets" -maxdepth 1 -name "*.jpg" -print -quit)"; then
-#    svn propset svn:mime-type "image/jpeg" "$SVN_DIR/assets/*.jpg" || true
-#fi
-#if test -d "$SVN_DIR/assets" && test -n "$(find "$SVN_DIR/assets" -maxdepth 1 -name "*.gif" -print -quit)"; then
-#    svn propset svn:mime-type "image/gif" "$SVN_DIR/assets/*.gif" || true
-#fi
-#if test -d "$SVN_DIR/assets" && test -n "$(find "$SVN_DIR/assets" -maxdepth 1 -name "*.svg" -print -quit)"; then
-#    svn propset svn:mime-type "image/svg+xml" "$SVN_DIR/assets/*.svg" || true
-#fi
-#
-#svn status
-#
-#echo "➤ Committing files..."
-#svn commit -m "$SVN_TEXT_COMMIT from GitHub" --no-auth-cache --non-interactive  --username "$SVN_USERNAME" --password "$SVN_PASSWORD"
-#
-#if $INPUT_GENERATE_ZIP; then
-#  echo "Generating zip file..."
-#  cd "$SVN_DIR/trunk" || exit
-#  zip -r "${GITHUB_WORKSPACE}/${SLUG}.zip" .
-#  echo "::set-output name=zip-path::${GITHUB_WORKSPACE}/${SLUG}.zip"
-#  echo "✓ Zip file generated!"
-#fi
-#
-#echo "✓ Plugin deployed!"
+
+echo "ℹ︎ VERSION is $VERSION"
+
+if [[ -z "$ASSETS_DIR" ]]; then
+	ASSETS_DIR=".wordpress-org"
+fi
+echo "ℹ︎ ASSETS_DIR is $ASSETS_DIR"
+
+SVN_URL="https://plugins.svn.wordpress.org/${SLUG}/"
+SVN_DIR="${HOME}/svn-${SLUG}"
+
+echo "ℹ︎ SVN url is $SVN_URL"
+echo "ℹ︎ SVN dir is $SVN_DIR"
+
+# Checkout just trunk and assets for efficiency
+# Tagging will be handled on the SVN level
+
+echo "➤ Checking out .org repository..."
+svn checkout --depth immediates "$SVN_URL" "$SVN_DIR"
+cd "$SVN_DIR"
+svn update --set-depth infinity assets
+svn update --set-depth infinity trunk
+
+echo "➤ Copying files..."
+if [[ -e "$GITHUB_WORKSPACE/.distignore" ]]; then
+	echo "ℹ︎ Using .distignore"
+	# Copy from current branch to /trunk, excluding dotorg assets
+	# The --delete flag will delete anything in destination that no longer exists in source
+	rsync -rc --exclude-from="$GITHUB_WORKSPACE/.distignore" "$GITHUB_WORKSPACE/" trunk/ --delete --delete-excluded
+else
+	echo "ℹ︎ Using .gitattributes"
+
+	cd "$GITHUB_WORKSPACE"
+
+	# "Export" a cleaned copy to a temp directory
+	TMP_DIR="${HOME}/archivetmp"
+	mkdir "$TMP_DIR"
+
+	git config --global user.email "vad3724@gmail.com"
+	git config --global user.name "vad3724"
+
+	# If there's no .gitattributes file, write a default one into place
+	if [[ ! -e "$GITHUB_WORKSPACE/.gitattributes" ]]; then
+		cat > "$GITHUB_WORKSPACE/.gitattributes" <<-EOL
+		/$ASSETS_DIR export-ignore
+		/.gitattributes export-ignore
+		/.gitignore export-ignore
+		/.github export-ignore
+		EOL
+
+		# Ensure we are in the $GITHUB_WORKSPACE directory, just in case
+		# The .gitattributes file has to be committed to be used
+		# Just don't push it to the origin repo :)
+		git add .gitattributes && git commit -m "Add .gitattributes file"
+	fi
+
+	# This will exclude everything in the .gitattributes file with the export-ignore flag
+	git archive HEAD | tar x --directory="$TMP_DIR"
+
+	cd "$SVN_DIR"
+
+	# Copy from clean copy to /trunk, excluding dotorg assets
+	# The --delete flag will delete anything in destination that no longer exists in source
+	rsync -rc "$TMP_DIR/" trunk/ --delete --delete-excluded
+fi
+
+# Copy dotorg assets to /assets
+if [[ -d "$GITHUB_WORKSPACE/$ASSETS_DIR/" ]]; then
+	rsync -rc "$GITHUB_WORKSPACE/$ASSETS_DIR/" assets/ --delete
+else
+	echo "ℹ︎ No assets directory found; skipping asset copy"
+fi
+
+# Add everything and commit to SVN
+# The force flag ensures we recurse into subdirectories even if they are already added
+# Suppress stdout in favor of svn status later for readability
+echo "➤ Preparing files..."
+svn add . --force > /dev/null
+
+# SVN delete all deleted files
+# Also suppress stdout here
+svn status | grep '^\!' | sed 's/! *//' | xargs -I% svn rm %@ > /dev/null
+
+# Copy tag locally to make this a single commit
+echo "➤ Copying tag..."
+svn cp "trunk" "tags/$SVN_VERSION"
+
+# Fix screenshots getting force downloaded when clicking them
+# https://developer.wordpress.org/plugins/wordpress-org/plugin-assets/
+if test -d "$SVN_DIR/assets" && test -n "$(find "$SVN_DIR/assets" -maxdepth 1 -name "*.png" -print -quit)"; then
+    svn propset svn:mime-type "image/png" "$SVN_DIR/assets/*.png" || true
+fi
+if test -d "$SVN_DIR/assets" && test -n "$(find "$SVN_DIR/assets" -maxdepth 1 -name "*.jpg" -print -quit)"; then
+    svn propset svn:mime-type "image/jpeg" "$SVN_DIR/assets/*.jpg" || true
+fi
+if test -d "$SVN_DIR/assets" && test -n "$(find "$SVN_DIR/assets" -maxdepth 1 -name "*.gif" -print -quit)"; then
+    svn propset svn:mime-type "image/gif" "$SVN_DIR/assets/*.gif" || true
+fi
+if test -d "$SVN_DIR/assets" && test -n "$(find "$SVN_DIR/assets" -maxdepth 1 -name "*.svg" -print -quit)"; then
+    svn propset svn:mime-type "image/svg+xml" "$SVN_DIR/assets/*.svg" || true
+fi
+
+svn status
+
+echo "➤ Committing files..."
+svn commit -m "$SVN_TEXT_COMMIT from GitHub" --no-auth-cache --non-interactive  --username "$SVN_USERNAME" --password "$SVN_PASSWORD"
+
+if $INPUT_GENERATE_ZIP; then
+  echo "Generating zip file..."
+  cd "$SVN_DIR/trunk" || exit
+  zip -r "${GITHUB_WORKSPACE}/${SLUG}.zip" .
+  echo "::set-output name=zip-path::${GITHUB_WORKSPACE}/${SLUG}.zip"
+  echo "✓ Zip file generated!"
+fi
+
+echo "✓ Plugin deployed!"
 
 
 
